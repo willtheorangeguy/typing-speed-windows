f9fd7c68726a72b2116981d040b774a0557684bd
William <willtheorangeguy@users.noreply.github.com>
2025-07-04 17:39:32 -0700

    Add files via upload

---
+++ MeasureModel.cs
@@ -0,0 +1,11 @@
+
+using System;
+
+namespace WPM_Tracker
+{
+    public class MeasureModel
+    {
+        public long DateTimeTicks { get; set; }
+        public double WpmValue { get; set; }
+    }
+}
